304550e84a5cada7aeca02988dc53edec19eab5e
Valentin Cedeño <89623002+SSMTBPSSCNTPGTS@users.noreply.github.com>
2021-09-23 14:26:56 -0300

    Update Midi_Controller_UNO.ino

--- Midi_Controller_UNO.ino
+++ Midi_Controller_UNO.ino
@@ -40,7 +40,7 @@ int pot_cc[]={0,1,56,57,58,59,60,61,62,63,64,65,67,68,69,70};// MIDI CC values f
 int pot_old_value[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 int pot_new_value[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-//******************************************* PINMODES **************************************************************
+//******************************************* Pin Modes *************************************************************
 pinMode(input,INPUT); // Analog input.
 
 pinMode(pin_0_bin, OUTPUT);// Multiplexer's address: Bit 0
@@ -100,97 +100,97 @@ bool b_6_new = digitalRead(b_6);
 bool b_7_new = digitalRead(b_7);
 bool b_8_new = digitalRead(b_8);
  
-if (b_1_new != b_1_old){
+if(b_1_new != b_1_old){
 	
-	if (b_1_old == LOW){
+	if(b_1_old == LOW){
 		MIDI.sendNoteOn(60, 127, 1); // Note C5 On - MIDI Value: 127 - MIDI Channel: 1
 	}
 	else{
-		MIDI.sendNoteOff(60, 0, 1);
+		MIDI.sendNoteOff(60, 0, 1); // Note C5 Off - MIDI Value: 0 - MIDI Channel: 1
 	}
 	
 	b_1_old = b_1_new;
 }
 
-if (b_2_new != b_2_old){
+if(b_2_new != b_2_old){
 	
-	if (b_2_new == LOW){
-		MIDI.sendNoteOn(62, 127, 1);
+	if(b_2_new == LOW){
+		MIDI.sendNoteOn(62, 127, 1);// Note D5 On - MIDI Value: 127 - MIDI Channel: 1
 	}
 	else{
-		MIDI.sendNoteOff(62, 0, 1);
+		MIDI.sendNoteOff(62, 0, 1);// Note D5 Off - MIDI Value: 0 - MIDI Channel: 1
 	}
 	
 	b_2_old = b_2_new;
 }
 
-if (b_3_new != b_3_old){
+if(b_3_new != b_3_old){
 	
-	if (b_3_new == LOW){
-		MIDI.sendNoteOn(64, 127, 1);
+	if(b_3_new == LOW){
+		MIDI.sendNoteOn(64, 127, 1);// Note E5 On - MIDI Value: 127 - MIDI Channel: 1
 	}
 	else{
-		MIDI.sendNoteOff(64, 0, 1);
+		MIDI.sendNoteOff(64, 0, 1);// Note E5 Off - MIDI Value: 0 - MIDI Channel: 1
 	}
 	
 	b_3_old = b_3_new;
 }
 
-if (b_4_new != b_4_old){
+if(b_4_new != b_4_old){
 	
-	if (b_4_new == LOW){
-		MIDI.sendNoteOn(65, 127, 1);
+	if(b_4_new == LOW){
+		MIDI.sendNoteOn(65, 127, 1);// Note F5 On - MIDI Value: 127 - MIDI Channel: 1
 	}
 	else{
-		MIDI.sendNoteOff(65, 0, 1);
+		MIDI.sendNoteOff(65, 0, 1);// Note F5 Off - MIDI Value: 0 - MIDI Channel: 1
 	}
 	
 	b_4_old = b_4_new;
 }
 
-if (b_5_new != b_5_old){
-	if (b_5_new == LOW){
+if(b_5_new != b_5_old){
+	if(b_5_new == LOW){
 		
-		MIDI.sendNoteOn(67, 127, 1);
+		MIDI.sendNoteOn(67, 127, 1);// Note G5 On - MIDI Value: 127 - MIDI Channel: 1
 	}
 	else{
-		MIDI.sendNoteOff(67, 0, 1);
+		MIDI.sendNoteOff(67, 0, 1);// Note G5 Off - MIDI Value: 0 - MIDI Channel: 1
 	}
 	
 	b_5_old = b_5_new;
 }
 
-if (b_6_new != b_6_old){
-	if (b_6_new == LOW){
+if(b_6_new != b_6_old){
+	if(b_6_new == LOW){
 			
-		MIDI.sendNoteOn(69, 127, 1);
+		MIDI.sendNoteOn(69, 127, 1);// Note A5 On - MIDI Value: 127 - MIDI Channel: 1
 	}
 	else{
-		MIDI.sendNoteOff(69, 0, 1);
+		MIDI.sendNoteOff(69, 0, 1);// Note A5 Off - MIDI Value: 0 - MIDI Channel: 1
 	}
 	
 	b_6_old = b_6_new;
 }
 
-if (b_7_new != b_7_old){
-	if (b_7_new == LOW){
+if(b_7_new != b_7_old){
+	if(b_7_new == LOW){
 		
-		MIDI.sendNoteOn(71, 127, 1);
+		MIDI.sendNoteOn(71, 127, 1);// Note B5 On - MIDI Value: 127 - MIDI Channel: 1
 	}
 	else{
-		MIDI.sendNoteOff(71, 0, 1);
+		MIDI.sendNoteOff(71, 0, 1);// Note B5 Off - MIDI Value: 0 - MIDI Channel: 1
 	}
 	
 	b_7_old = b_7_new;
 }
 
-if (b_8_new != b_8_old){
-	if (b_8_new == LOW){
+if(b_8_new != b_8_old){
+	if(b_8_new == LOW){
 		
-		MIDI.sendNoteOn(72, 127, 1);
+		MIDI.sendNoteOn(72, 127, 1);// Note C6 On - MIDI Value: 127 - MIDI Channel: 1
 	}
 	else{
-		MIDI.sendNoteOff(72, 0, 1);
+		MIDI.sendNoteOff(72, 0, 1);// Note C6 On - MIDI Value: 0 - MIDI Channel: 1
 	}
 	
 	b_8_old = b_8_new;
